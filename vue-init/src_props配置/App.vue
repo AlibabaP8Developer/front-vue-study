df96775dc706d2ef488403724155f4db2493017d
Edwin <1135365441@qq.com>
2023-04-15 12:50:19 +0800

    Vue脚手架基础

---
+++ vue-init/src_props配置/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <h1 v-text="msg" ref="title"></h1>
+    <button @click="showDOM">点我输出上方的dom元素</button>
+    <student ref="sc" name="完颜璟" address="上京会宁府" :age="10"></student>
+    <student name="完颜雍" address="燕京" ></student>
+    <student name="爱新觉罗弘历" address="北平" :age="11"></student>
+  </div>
+</template>
+
+<script>
+import Student from "@/components/Student";
+
+export default {
+  name: 'App',
+  components: {
+    Student
+  },
+  data() {
+    return {
+      msg: '欢迎学习Vue！'
+    }
+  },
+  methods: {
+    showDOM() {
+      console.log('---', this.$refs)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-init/src_props配置/components/Student.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="school">
+    <h2>学校的名称：{{ name }}</h2>
+    <h2>学校的成立年份：{{ myAge }}</h2>
+    <h2>学校的地址：{{ address }}</h2>
+    <button @click="updateAge">尝试修改收到的年龄</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'School',
+  //props: ['name', 'address'],
+  //接收的同时对数据进行类型限制
+  // props: {
+  //   name: String,
+  //   year: Number,
+  //   address: String
+  // },
+  props: {
+    name: {
+      type: String,
+      required: true  // 必须传的
+    },
+    age: {
+      type: Number,
+      default: 99
+    },
+    address: {
+      type: String,
+      required: true  // 必须传的
+    }
+  },
+  data() {
+    return {
+      myAge: this.age
+    }
+  },
+  methods: {
+    updateAge() {
+      this.myAge++
+    }
+  }
+}
+</script>
+
+<style scoped>
+.school {
+  background-color: gray;
+}
+</style>
