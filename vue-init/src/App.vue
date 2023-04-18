8ddcf76ca9b82c82e62680b2b402649590050433
Edwin <1135365441@qq.com>
2023-04-18 20:12:58 +0800

    Vue基础(基于脚手架)

--- vue-init/src/App.vue
+++ vue-init/src/App.vue
@@ -1,23 +1,27 @@
 <template>
   <div id="app">
-    <MyHeader></MyHeader>
-    <List></List>
-    <MyFooter></MyFooter>
+    <div class="todo-container">
+      <div class="todo-wrap">
+        <MyHeader></MyHeader>
+        <MyList></MyList>
+        <MyFooter></MyFooter>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import MyHeader from "@/components/MyHeader";
-import List from "@/components/MyList";
-import MyFooter from "@/components/MyFooter";
 
+import MyHeader from "@/components/MyHeader";
+import MyList from "@/components/MyList";
+import MyFooter from "@/components/MyFooter";
 
 export default {
   name: 'App',
   components: {
     MyFooter,
-    MyHeader,
-    List
+    MyList,
+    MyHeader
   },
   data() {
     return {}
@@ -27,5 +31,46 @@ export default {
 </script>
 
 <style>
+/*base*/
+body {
+  background: #f5f1f1;
+}
 
+.btn {
+  display: inline-block;
+  padding: 4px 12px;
+  margin-bottom: 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  vertical-align: middle;
+  cursor: pointer;
+  box-shadow: inset 0 1px 0 rgba(255, 255, 255, 0.2), 0 1px 2px rgba(0, 0, 0, 0.05);
+  border-radius: 4px;
+}
+
+.btn-danger {
+  color: #fff;
+  background-color: #da4f49;
+  border: 1px solid #bd362f;
+}
+
+.btn-danger:hover {
+  color: #fff;
+  background-color: #bd362f;
+}
+
+.btn:focus {
+  outline: none;
+}
+
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
 </style>
--- vue-init/src/components/MyList.vue
+++ vue-init/src/components/MyList.vue
@@ -1,20 +1,48 @@
 <template>
   <div>
-    <Item></Item>
+    <ul class="todo-main">
+      <Item v-for="todoObj in todos" :key="todoObj.id" :todo="todoObj"></Item>
+    </ul>
   </div>
 </template>
 
 <script>
-import Item from "@/components/Item";
 
+import Item from "@/components/Item";
 export default {
   name: "MyList",
-  components: {
-    Item
+  components: {Item},
+  data() {
+    return {
+      todos: [
+        {id: '1001', title: '朱元璋', done: true},
+        {id: '1002', title: '完颜亮', done: true},
+        {id: '1004', title: '阿敏', done: true},
+        {id: '1005', title: '莽古尔泰', done: true},
+        {id: '1006', title: '喇嘛达尔扎', done: true},
+        {id: '1007', title: '葛尔丹策零', done: true},
+        {id: '1003', title: '朱重八', done: false}
+      ]
+    }
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
 
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
 </style>
