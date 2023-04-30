8b25c7ea000400468571a1357496a09ac1a38ede
Edwin <1135365441@qq.com>
2023-04-30 22:19:05 +0800

    Vue基础(基于脚手架)

--- vue-init/src/App.vue
+++ vue-init/src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="app">
-    <h1>{{msg}}</h1>
+    <h1>{{ msg }}</h1>
     <!-- 通过父组件给子组件传递函数类型的props实现：子给父传递数据 -->
     <school :getSchoolName="getSchoolName"></school>
-    <student v-on:atguigu="getStudentName"></student>
+    <!--@或v-on  once一次性-->
+    <student @atguigu="getStudentName" @demo="m1"></student>
+    <!--ref-->
+    <student ref="student"></student>
   </div>
 </template>
 
@@ -22,12 +25,21 @@ export default {
       msg: '欢迎学习Vue！'
     }
   },
+  mounted() {
+    setTimeout(() => {
+      this.$refs.student.$on('atguigu', this.getStudentName) // 绑定自定义事件
+      // this.$refs.student.$once('atguigu', this.getStudentName) // 绑定自定义事件 【一次性】
+    }, 3000)
+  },
   methods: {
     getSchoolName(name) {
       console.log('App收到了学校名：', name)
     },
-    getStudentName(name) {
-      console.log('App收到了学生名', name)
+    getStudentName(name, ...params) {
+      console.log('App收到了学生名', name, params)
+    },
+    m1(){
+      console.log('demo事件被触发了')
     }
   }
 }
--- vue-init/src/components/Student.vue
+++ vue-init/src/components/Student.vue
@@ -3,6 +3,7 @@
     <h2>学生的姓名：{{ name }}</h2>
     <h2>学生的地址：{{ sex }}</h2>
     <button @click="sendStudentName">把学生名给App</button>
+    <button @click="unbind">解绑atguigu事件</button>
   </div>
 </template>
 
@@ -18,7 +19,13 @@ export default {
   methods: {
     sendStudentName() {
       // 触发student组件实例身上的atguigu事件
-      this.$emit('atguigu', this.name)
+      this.$emit('atguigu', this.name, this.sex)
+      this.$emit('demo')
+    },
+    unbind() {
+      // this.$off('atguigu')  // 解绑一个自定义事件
+      // this.$off(['atguigu', 'demo'])  // 解绑多个自定义事件
+      this.$off() //  // 解绑全部自定义事件
     }
   }
 }
