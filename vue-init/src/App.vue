8c482336d9827d61f99df0162534277d17a2173b
Edwin <1135365441@qq.com>
2023-05-02 00:47:51 +0800

    Vue基础(基于脚手架)

--- vue-init/src/App.vue
+++ vue-init/src/App.vue
@@ -1,23 +1,40 @@
 <template>
-  <div>
-    <Test></Test>
-    <Test2></Test2>
-    <Test3></Test3>
+  <div class="container">
+    <Category title="美食">
+      <img src="@/assets/guobaorou.jpeg">
+    </Category>
+    <Category title="游戏">
+      <ul v-for="(item, index) in games" :key="index">
+        <li>{{ item }}</li>
+      </ul>
+    </Category>
+    <Category title="电影">
+      <img src="@/assets/menglong.jpg">
+    </Category>
   </div>
 </template>
 
 <script>
-import Test from "@/components/Test";
-import Test2 from "@/components/Test2";
-import Test3 from "@/components/Test3";
+import Category from "@/components/Category";
 
 export default {
   name: 'App',
   components: {
-    Test,
-    Test2,
-    Test3
+    Category
+  },
+  data() {
+    return {
+      foods: ['铜火锅', '酸菜粉', '红烧肉', '锅包肉'],
+      games: ['坦克大战', '超级玛丽', '王者荣耀', '英雄联盟'],
+      films: ['《即日启程》','《神通乡巴佬》','《唐山大兄》','《精武门》']
+    }
   }
 }
 </script>
 
+<style lang="css">
+.container {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
---
+++ vue-init/src/components/Category.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="category">
+    <h3>{{title}}分类</h3>
+    <!-- 定义一个插槽，使用者进行填充 -->
+    <slot>定义一个插槽</slot>
+<!--
+图片：https://s3.ax1x.com/2021/01/16/srJ1q0.jpg
+视频：
+-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  props: ['listData', 'title']
+}
+</script>
+
+<style scoped>
+.category {
+  background-color: skyblue;
+  width: 200px;
+  height: 300px;
+}
+
+h3 {
+  text-align: center;
+  background-color: orange;
+}
+
+img {
+  width: 100%;
+  height: 200px;
+}
+</style>
