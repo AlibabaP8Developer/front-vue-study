d0a828afa7d02b41c87b799470d586f549a567b2
Edwin <1135365441@qq.com>
2023-05-02 16:57:20 +0800

    Vue基础(基于脚手架)

--- vue-init/src/components/Count.vue
+++ vue-init/src/components/Count.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>当前求和为：{{sum}}</h1>
+    <h1>当前求和为：{{this.$store.state.sum}}</h1>
     <select v-model="n">
       <option :value="1">1</option>
       <option :value="2">2</option>
@@ -20,24 +20,23 @@ export default {
   data() {
     return {
       n: 1, // 用户选择的数字
-      sum: 0 // 当前的和
     }
+  },
+  mounted() {
+    console.log('vc:', this)
   },
   methods: {
     increment() {
-      this.sum += this.n
+      this.$store.dispatch('jia', this.n)
     },
     decrement() {
-      this.sum -= this.n
     },
     incrementOdd() {
-      if (this.sum % 2) {
-        this.sum += this.n
-      }
+
     },
     incrementWait() {
-      setTimeout(()=>{
-        this.sum += this.n
+      setTimeout(() => {
+
       }, 500)
     }
   }
--- vue-init/src/store/index.js
+++ vue-init/src/store/index.js
@@ -4,21 +4,28 @@
 import Vue from 'vue'
 // 引入Vuex
 import Vuex from 'vuex'
+
 Vue.use(Vuex)
 
 // 准备actions，用于响应组件中的动作
 const actions = {
-
+    jia(context, value) {
+        // console.log('actions中的jia调用了', context, value)
+        context.commit('JIA', value)
+    }
 }
 
 // 准备mutations，用于操作数据（state）
 const mutations = {
-
+    JIA(state, value) {
+        console.log('actions中的JIA被调用了', state, value)
+        state.sum += value
+    }
 }
 
 // 准备state，用于存储数据
 const state = {
-
+    sum: 0 //  当前的和
 }
 
 // 创建store，导出store
