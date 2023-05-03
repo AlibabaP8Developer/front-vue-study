4131920f622abe66cf162883b5dcb79022c1a852
Edwin <1135365441@qq.com>
2023-05-03 20:49:56 +0800

    Vue基础(基于脚手架)

--- vue-init/src/components/Count.vue
+++ vue-init/src/components/Count.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h1>当前求和为：{{this.$store.state.sum}}</h1>
+    <h1>当前求和为：{{ sum }}</h1>
+    <h1>当前求和放大10倍为：{{ bigSum }}</h1>
+    <h1>完颜麻达葛在{{ school }}，学习{{ subject }}</h1>
     <select v-model="n">
       <option :value="1">1</option>
       <option :value="2">2</option>
@@ -14,6 +16,8 @@
 </template>
 
 <script>
+import {mapState, mapGetters} from 'vuex'
+
 export default {
   name: "Category",
   props: ['title'],
@@ -22,22 +26,66 @@ export default {
       n: 1, // 用户选择的数字
     }
   },
+  computed: {
+    /*sum() {
+      return this.$store.state.sum
+    },
+    school() {
+      return this.$store.state.school
+    },
+    subject() {
+      return this.$store.state.subject
+    }*/
+
+    // 借助mapState生成上述计算属性，从state中读取数据（对象写法）
+    // ...mapState({
+    //   sum: 'sum',
+    //   school: 'school',
+    //   subject: 'subject'
+    // }),
+
+    // 数组写法
+    ...mapState(['sum',
+      'school',
+      'subject'
+    ]),
+
+    // bigSum() {
+    //   return this.$store.getters.bigSum
+    // },
+
+    // 借助mapGetters生成计算属性，从getters中读取数据（对象写法）
+    // ...mapGetters({
+    //   bigSum: 'bigSum'
+    // }),
+    // （数组写法）
+    ...mapGetters([
+      'bigSum'
+    ])
+  },
   mounted() {
-    console.log('vc:', this)
+    console.log('vc:', this.$store)
+    const x = mapState({
+      sum: 'sum',
+      school: 'school',
+      subject: 'subject'
+    })
+    console.log('x: ', x)
   },
   methods: {
     increment() {
-      this.$store.dispatch('jia', this.n)
+      this.$store.commit('JIA', this.n)
     },
     decrement() {
+      this.$store.commit('JIAN', this.n)
     },
     incrementOdd() {
-
+      if (this.$store.state.sum % 2) {
+        this.$store.dispatch('jiaOdd', this.n)
+      }
     },
     incrementWait() {
-      setTimeout(() => {
-
-      }, 500)
+      this.$store.dispatch('jiaWait', this.n)
     }
   }
 }
--- vue-init/src/store/index.js
+++ vue-init/src/store/index.js
@@ -12,6 +12,21 @@ const actions = {
     jia(context, value) {
         // console.log('actions中的jia调用了', context, value)
         context.commit('JIA', value)
+    },
+    jian(context, value) {
+        // console.log('actions中的jian调用了', context, value)
+        context.commit('JIAN', value)
+    },
+    jiaOdd(context, value) {
+        // console.log('actions中的jia调用了', context, value)
+        if (context.state.sum % 2) {
+            context.commit('JIAODD', value)
+        }
+    },
+    jiaWait(context, value) {
+        setTimeout(() => {
+            context.commit('JIAWAIT', value)
+        }, 500)
     }
 }
 
@@ -20,12 +35,33 @@ const mutations = {
     JIA(state, value) {
         console.log('actions中的JIA被调用了', state, value)
         state.sum += value
-    }
+    },
+    JIAN(state, value) {
+        console.log('actions中的JIAN被调用了', state, value)
+        state.sum -= value
+    },
+    JIAODD(state, value) {
+        console.log('actions中的JIAODD被调用了', state, value)
+        state.sum += value
+    },
+    JIAWAIT(state, value) {
+        console.log('actions中的JIAWAIT被调用了', state, value)
+        state.sum += value
+    },
 }
 
 // 准备state，用于存储数据
 const state = {
-    sum: 0 //  当前的和
+    sum: 0, //  当前的和
+    school: '尚硅谷',
+    subject: '前端'
+}
+
+// 准备getter，用于将state中的数据进行加工
+const getters = {
+    bigSum(state) {
+        return state.sum * 10
+    }
 }
 
 // 创建store，导出store
@@ -33,4 +69,5 @@ export default new Vuex.Store({
     actions,
     mutations,
     state,
+    getters
 })
