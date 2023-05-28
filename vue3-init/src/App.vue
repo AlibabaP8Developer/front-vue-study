8e0aae0ac0342c0120f299db0bb747a680c1aa9f
Edwin <1135365441@qq.com>
2023-05-28 22:58:01 +0800

    vue3基础学习

--- vue3-init/src/App.vue
+++ vue3-init/src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <Demo/>
+  <button @click="isShowDemo=!isShowDemo">切换隐藏/显示</button>
+  <Demo v-if="isShowDemo"/>
 </template>
 
 <script>
 import Demo from '@/components/Demo'
+import {ref} from 'vue'
 
 export default {
   name: 'App',
@@ -11,8 +13,10 @@ export default {
   // 此处只是测试setup
   setup () {
 
-    return {
+    let isShowDemo = ref(true)
 
+    return {
+      isShowDemo
     }
 
   },
--- vue3-init/src/components/Demo.vue
+++ vue3-init/src/components/Demo.vue
@@ -3,58 +3,41 @@
     <h1>当前求和为：{{ sum }}</h1>
     <button @click="sum++">点我加1</button>
     <hr>
-    <h2>当前的信息为：{{ msg }}</h2>
-    <button @click="msg+='!'">修改信息</button>
-    <hr>
-    <h2>姓名：{{ person.name }}</h2>
-    <h2>年龄：{{ person.age }}</h2>
-    <h2>工钱：{{ person.job.j1.salary }}</h2>
-    <button @click="person.name+='，耶律阿保机的后人'">修改姓名</button>
-    <button @click="person.age++">增长年龄</button>
-    <button @click="person.job.j1.salary++">增长工钱</button>
+    <h2>当前点击时鼠标的坐标为：x: {{ point.x }}，y: {{ point.y }}</h2>
   </div>
 </template>
 
 <script>
-import { watch, ref, watchEffect, reactive } from 'vue'
+import { watch, ref, onMounted, reactive, onBeforeUnmount } from 'vue'
 
 export default {
   name: 'Demo',
   setup () {
     let sum = ref(0)
-    let msg = ref('你好，耶律大石')
-    let person = reactive({
-      name: '耶律楚材',
-      age: 18,
-      job: {
-        j1: {
-          salary: 10000
-        }
-      }
+
+    let point = reactive({
+      x: 0,
+      y: 0
     })
 
-    watch(sum, (newValue, oldValue) => {
-      console.log('sum的值变了：',newValue, oldValue)
-    }, {
-      immediate: true
+    function savePoint (event) {
+      point.x = event.pageX
+      point.y = event.pageY
+      console.log(event.pageX, '-', event.pageY)
+    }
+
+    onMounted(() => {
+      window.addEventListener('click', savePoint)
     })
 
-    // watch(person.value, (newValue, oldValue) => {
-    //   console.log('person的值变了：',newValue, oldValue)
-    // })
-
-    // 监视 watchEffect用谁监视谁
-    watchEffect( () => {
-      const x = sum.value
-      const y = person.job.j1.salary
-      console.log('watchEffect所指定的回调执行了')
+    onBeforeUnmount(() => {
+      window.removeEventListener('click', savePoint)
     })
 
     // 返回一个对象（常用）
     return {
       sum,
-      msg,
-      person
+      point
     }
   }
 }
