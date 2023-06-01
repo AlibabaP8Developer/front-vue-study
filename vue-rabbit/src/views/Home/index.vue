34d4902675384ab24d6300ec10f9dc8e2604667a
Edwin <1135365441@qq.com>
2023-06-01 22:48:24 +0800

    小兔鲜项目

--- vue-rabbit/src/views/Home/index.vue
+++ vue-rabbit/src/views/Home/index.vue
@@ -1,11 +1,19 @@
 <template>
-  这个是首页
-  <div style="height: 100%">
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
   </div>
+  <HomeNew />
+  <HomeHot />
+  <HomeProduct />
 </template>
 
 <script setup>
-
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import HomeProduct from './components/HomeProduct.vue'
 </script>
 
 <style scoped lang="scss">
