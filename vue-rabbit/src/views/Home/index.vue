9f4b26548715c40cf1762cdb6190de6a11019c64
Edwin <1135365441@qq.com>
2023-06-03 11:14:42 +0800

    小兔鲜项目封装面板组件

--- vue-rabbit/src/views/Home/index.vue
+++ vue-rabbit/src/views/Home/index.vue
@@ -6,6 +6,16 @@
   <HomeNew />
   <HomeHot />
   <HomeProduct />
+  <home-panel title="新鲜好物" sub-title="新鲜好物 好多商品">
+    <div>
+      人气推荐
+    </div>
+  </home-panel>
+  <home-panel title="人气推荐" sub-title="人气推荐 好多商品">
+    <div>
+      人气推荐
+    </div>
+  </home-panel>
 </template>
 
 <script setup>
@@ -14,6 +24,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import HomeProduct from './components/HomeProduct.vue'
+import HomePanel from "./components/HomePanel.vue";
 </script>
 
 <style scoped lang="scss">
