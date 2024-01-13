0d2092ccd720fcd1cca2553023fe9efd1d941143
Edwin <1135365441@qq.com>
2024-01-13 23:54:51 +0800

    自定义导航栏

--- heima-shop/src/pages/index/index.vue
+++ heima-shop/src/pages/index/index.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import CustomNavbar from "@/pages/index/components/CustomNavbar.vue";
+import XxSwiper from '@/components/XxSwiper.vue'
 </script>
 
 <template>
   <CustomNavbar></CustomNavbar>
+  <XxSwiper></XxSwiper>
 </template>
 
 <style lang="scss">
